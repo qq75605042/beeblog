a7190211c72a0aa3618a6d6193b55622c63eae3b
yookie <yookie@qq.com>
2018-12-22 15:06:56 +0800

    orm log write to file

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
 
 func main() {
 	_ = orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/blog?charset=utf8&loc=Asia%2FShanghai", 30)
 	// 开启调试模式,打印sql
 	orm.Debug = true
-	_ = logs.SetLogger(logs.AdapterFile,`{"filename":"sql.log"}`)
+	w,_ := os.OpenFile("./logs/sql.log",os.O_RDWR|os.O_CREATE,0664)
+	defer w.Close()
+	orm.DebugLog = orm.NewLog(w)
+	_ = logs.SetLogger(logs.AdapterFile,`{"filename":"logs/err.log"}`)
 	//orm.RegisterModel(new(models.Article))
 	//o:= orm.NewOrm()
 	//art := models.Article{Id:int64(1)}
